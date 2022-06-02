ff3bebd43a4dc989e49c3b515d10f80ae3fa930c
nicolasrojasvass <nicolas.rojas@vasslatam.com>
2022-06-02 17:00:20 -0500

    Cambio de metodos asociados ahora a la BD

--- ProjectSprint1/src/main/java/com/springboot/project/ProjectSprint1/service/ImplUserService.java
+++ ProjectSprint1/src/main/java/com/springboot/project/ProjectSprint1/service/ImplUserService.java
@@ -1,54 +1,64 @@
 package com.springboot.project.ProjectSprint1.service;
 
 import com.springboot.project.ProjectSprint1.model.User;
+import com.springboot.project.ProjectSprint1.repository.IUserRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+
 @Service
 public class ImplUserService implements IUserService {
-    private final List<User> listUsers = new ArrayList<>();
+    private final IUserRepository iUserRepository;
 
+    public ImplUserService(IUserRepository iUserRepository) {
+        this.iUserRepository = iUserRepository;
+    }
 
     @Override
     public User createUser(User user) {
-        listUsers.add(user);
-        return listUsers.stream().filter(element -> element.getIdUser().equals(user.getIdUser())).collect(Collectors.toList()).get(0);
+        return iUserRepository.save(user);
     }
 
     @Override
     public Optional<User> findByIdUser(String idUser) {
-        User user = listUsers.stream().filter(element -> element.getIdUser().equals(idUser)).collect(Collectors.toList()).get(0);
-        return user != null ? Optional.of(user) : Optional.empty();
+        try {
+            return iUserRepository.findById(Long.parseLong(idUser));
+        } catch (NumberFormatException e) {
+            e.getMessage();
+            return Optional.empty();
+        }
     }
 
     @Override
     public Optional<List<User>> getAllUsers() {
+        List<User> listUsers = iUserRepository.findAll();
         return (listUsers != null && !listUsers.isEmpty()) ? Optional.of(listUsers) : Optional.empty();
     }
 
     @Override
     public boolean deleteUser(String idUser) {
-        listUsers.removeIf((s) -> s.getIdUser().equals(idUser));
-        return !listUsers.stream().anyMatch(element -> element.getIdUser().equals(idUser));
+        try {
+            Long id = Long.parseLong(idUser);
+            iUserRepository.deleteById(id);
+            Optional<User> user = iUserRepository.findById(id);
+            return user.isPresent() ? false : true;
+        } catch (NumberFormatException e) {
+             e.getMessage();
+             return false;
+        }
     }
 
     @Override
     public Optional<User> updateUser(String idUser, User userUpdated) {
-        try {
-            int index = IntStream.range(0, listUsers.size())
-                    .filter(userIndex -> listUsers.get(userIndex).getIdUser().equals(idUser))
-                    .findFirst().getAsInt();
-            listUsers.get(index).setEmail(userUpdated.getEmail());
-            listUsers.get(index).setFirstName(userUpdated.getFirstName());
-            listUsers.get(index).setLastName(userUpdated.getLastName());
-            listUsers.get(index).setUsername(userUpdated.getUsername());
-            return Optional.of(listUsers.get(index));
-        } catch (NoSuchElementException e) {
+        Optional<User> user = iUserRepository.findById(Long.parseLong(idUser));
+        if (user.isPresent()){
+            user.get().setEmail(userUpdated.getEmail());
+            user.get().setFirst_name(userUpdated.getFirst_name());
+            user.get().setLast_name(userUpdated.getLast_name());
+            user.get().setUsername(userUpdated.getUsername());
+            return Optional.of(iUserRepository.save(user.get()));
+        } else {
             return Optional.empty();
         }
     }
