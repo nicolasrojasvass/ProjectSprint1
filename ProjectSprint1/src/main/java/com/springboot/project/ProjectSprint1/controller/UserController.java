b2c441d2ef26f074d283f027e8f8e05eb08ebd79
nicolasrojasvass <nicolas.rojas@vasslatam.com>
2022-06-07 17:17:49 -0500

    Implementacion de ResponseEntity y cambio de endpoint a v2

--- ProjectSprint1/src/main/java/com/springboot/project/ProjectSprint1/controller/UserController.java
+++ ProjectSprint1/src/main/java/com/springboot/project/ProjectSprint1/controller/UserController.java
@@ -5,6 +5,8 @@ import com.springboot.project.ProjectSprint1.controller.dto.UserDTO;
 import com.springboot.project.ProjectSprint1.model.User;
 import com.springboot.project.ProjectSprint1.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -12,33 +14,33 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/v1/users")
+@RequestMapping("/v2/users")
 public class UserController {
     @Autowired
     IUserService iUserService;
 
     @PostMapping("/createUser")
-    public User createUser(@RequestBody UserDTO userDTO) {
-        return iUserService.createUser(new User(userDTO));
+    public ResponseEntity<User> createUser(@RequestBody UserDTO userDTO) {
+        return new ResponseEntity<>(iUserService.createUser(new User(userDTO)), HttpStatus.OK);
     }
 
     @GetMapping("/findById/{idUser}")
-    public Optional<User> findById(@PathVariable String idUser) {
-        return iUserService.findByIdUser(idUser);
+    public ResponseEntity<Optional<User>> findById(@PathVariable String idUser) {
+        return new ResponseEntity<>(iUserService.findByIdUser(idUser), HttpStatus.OK);
     }
 
     @GetMapping("/getAllUsers")
-    public Optional<List<User>> getAllUsers() {
-        return iUserService.getAllUsers();
+    public ResponseEntity<Optional<List<User>>> getAllUsers() {
+        return new ResponseEntity<>(iUserService.getAllUsers(), HttpStatus.OK);
     }
 
     @DeleteMapping("/deleteUser/{idUser}")
-    public boolean deleteUser(@PathVariable String idUser) {
-        return iUserService.deleteUser(idUser);
+    public ResponseEntity<Boolean> deleteUser(@PathVariable String idUser) {
+        return new ResponseEntity<>(iUserService.deleteUser(idUser), HttpStatus.OK);
     }
 
     @PutMapping("/updateUser/{idUser}")
-    public Optional<User> updateUser(@PathVariable String idUser, @RequestBody UserDTO userDTO) {
-        return iUserService.updateUser(idUser, new User(userDTO));
+    public ResponseEntity<Optional<User>> updateUser(@PathVariable String idUser, @RequestBody UserDTO userDTO) {
+        return new ResponseEntity<>(iUserService.updateUser(idUser, new User(userDTO)), HttpStatus.OK);
     }
 }
